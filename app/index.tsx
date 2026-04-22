952988b894b6ac502861904ac93bc120b4f846d5
codeaiden56-pixel <codeaiden56@gmail.com>
2026-04-21 19:16:27 -0700

    Added poem fetch

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+
+type Poem = {
+  author: string;
+  lines: string[];
+};
 
 export default function Index() {
+  const [poetry, setPoetry] = useState<Poem[]>([]);
+  const [linesShown, setLinesShown] = useState<number>(1);
+
+  const fetchPoetry = () => {
+    fetch("https://poetrydb.org/random")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPoetry(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchPoetry();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Guess the Poet</Text>
+      <ScrollView>
+        {poetry[0]?.lines.slice(0, linesShown).map((line) => (
+          <Text key={line}>{line}</Text>
+        ))}
+      </ScrollView>
     </View>
   );
 }
