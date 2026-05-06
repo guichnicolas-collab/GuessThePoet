cf38a7a922258cbf085643cce72c9a1fb19012ff
codeaiden56-pixel <codeaiden56@gmail.com>
2026-05-05 19:09:34 -0700

    Removed most duplicate poets

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,14 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 
 type Poem = {
   author: string;
   lines: string[];
 };
+
+function getRandomInteger(max: number) {
+  return Math.floor(Math.random() * max);
+}
 
 export default function Index() {
   const [poems, setPoems] = useState<Poem[]>([]);
@@ -14,13 +18,40 @@ export default function Index() {
   >();
   const [score, setScore] = useState<number>(0);
   const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
+  const fallbackPoetsRef = useRef<Set<string>>(
+    new Set([
+      "Amy Levy",
+      "John Milton",
+      "Geoffrey Chaucer",
+      "Alexander Pope",
+      "Percy Bysshe Shelley",
+      "Isaac Watts",
+      "Edgar Allan Poe",
+    ]),
+  );
 
   const fetchPoetry = useCallback(() => {
     fetch("https://poetrydb.org/random/4")
-      .then((response) => response.json())
+      .then((res) => res.json())
       .then((data) => {
+        const correctAnswerIndex = getRandomInteger(data.length);
+        for (let i = 0; i < data.length; i++) {
+          if (
+            i !== correctAnswerIndex &&
+            data.some(
+              (poem: Poem, index: number) =>
+                poem.author === data[i].author && index !== i,
+            )
+          ) {
+            const pool = [...fallbackPoetsRef.current];
+            data[i].author = pool[getRandomInteger(pool.length)];
+          }
+        }
+        for (const poem of data) {
+          fallbackPoetsRef.current.add(poem.author);
+        }
         setPoems(data);
-        setCorrectPoemIndex(Math.floor(Math.random() * data.length));
+        setCorrectPoemIndex(correctAnswerIndex);
       });
   }, []);
 
@@ -29,7 +60,13 @@ export default function Index() {
   }, [fetchPoetry]);
 
   const handleButtonPress = () => {
-    setLinesShown((prev) => prev + 1);
+    if (!correctPoemIndex) return;
+
+    if (poems[correctPoemIndex].lines[linesShown + 1]) {
+      setLinesShown((prev) => prev + 1);
+    } else {
+      setLinesShown((prev) => prev + 2);
+    }
   };
 
   const selectPoet = (index: number) => {
@@ -59,10 +96,13 @@ export default function Index() {
     );
   }
 
+  const isNewLineButtonEnabled =
+    linesShown < poems[correctPoemIndex].lines.length;
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Guess the Poet</Text>
-      <ScrollView>
+      <ScrollView style={{ marginBottom: 24 }}>
         {poems[correctPoemIndex].lines
           .slice(0, linesShown)
           .map((line, index) => (
@@ -80,7 +120,15 @@ export default function Index() {
           <Text>{poem.author}</Text>
         </Pressable>
       ))}
-      <Pressable onPress={handleButtonPress} style={styles.newLineButton}>
+      <Pressable
+        onPress={handleButtonPress}
+        style={
+          isNewLineButtonEnabled
+            ? styles.newLineButton
+            : styles.disabledNewLineButton
+        }
+        disabled={!isNewLineButtonEnabled}
+      >
         <Text style={styles.newLineText}>New Line</Text>
       </Pressable>
     </View>
@@ -108,6 +156,12 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     padding: 12,
     backgroundColor: "black",
+    borderRadius: 10,
+  },
+  disabledNewLineButton: {
+    alignSelf: "center",
+    padding: 12,
+    backgroundColor: "lightgray",
     borderRadius: 10,
   },
   newLineText: {
