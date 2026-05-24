29a7e93a91925a3a946fd1d39c5ff7eec634212b
Nicolas Guich <guichnicolas@gmail.com>
2026-05-24 08:36:23 -0700

    redo poem and poet generation

--- app/index.tsx
+++ app/index.tsx
@@ -6,53 +6,178 @@ type Poem = {
   lines: string[];
 };
 
-function getRandomInteger(max: number) {
-  return Math.floor(Math.random() * max);
-}
+const poets = [
+  "Adam Lindsay Gordon",
+  "Alan Seeger",
+  "Alexander Pope",
+  "Algernon Charles Swinburne",
+  "Ambrose Bierce",
+  "Amy Levy",
+  "Andrew Marvell",
+  "Ann Taylor",
+  "Anne Bradstreet",
+  "Anne Bronte",
+  "Anne Killigrew",
+  "Anne Kingsmill Finch",
+  "Annie Louisa Walker",
+  "Arthur Hugh Clough",
+  "Ben Jonson",
+  "Charles Kingsley",
+  "Charles Sorley",
+  "Charlotte Bronte",
+  "Charlotte Smith",
+  "Christina Rossetti",
+  "Christopher Marlowe",
+  "Christopher Smart",
+  "Coventry Patmore",
+  "Edgar Allan Poe",
+  "Edmund Spenser",
+  "Edward Fitzgerald",
+  "Edward Lear",
+  "Edward Taylor",
+  "Edward Thomas",
+  "Eliza Cook",
+  "Elizabeth Barrett Browning",
+  "Emily Bronte",
+  "Emily Dickinson",
+  "Emma Lazarus",
+  "Ernest Dowson",
+  "Eugene Field",
+  "Francis Thompson",
+  "Geoffrey Chaucer",
+  "George Eliot",
+  "George Gordon, Lord Byron",
+  "George Herbert",
+  "George Meredith",
+  "Gerard Manley Hopkins",
+  "Helen Hunt Jackson",
+  "Henry David Thoreau",
+  "Henry Vaughan",
+  "Henry Wadsworth Longfellow",
+  "Hugh Henry Brackenridge",
+  "Isaac Watts",
+  "James Henry Leigh Hunt",
+  "James Thomson",
+  "James Whitcomb Riley",
+  "Jane Austen",
+  "Jane Taylor",
+  "John Clare",
+  "John Donne",
+  "John Dryden",
+  "John Greenleaf Whittier",
+  "John Keats",
+  "John McCrae",
+  "John Milton",
+  "John Trumbull",
+  "John Wilmot",
+  "Jonathan Swift",
+  "Joseph Warton",
+  "Joyce Kilmer",
+  "Julia Ward Howe",
+  "Jupiter Hammon",
+  "Katherine Philips",
+  "Lady Mary Chudleigh",
+  "Lewis Carroll",
+  "Lord Alfred Tennyson",
+  "Louisa May Alcott",
+  "Major Henry Livingston, Jr.",
+  "Mark Twain",
+  "Mary Elizabeth Coleridge",
+  "Matthew Arnold",
+  "Matthew Prior",
+  "Michael Drayton",
+  "Oliver Goldsmith",
+  "Oliver Wendell Holmes",
+  "Oscar Wilde",
+  "Paul Laurence Dunbar",
+  "Percy Bysshe Shelley",
+  "Philip Freneau",
+  "Phillis Wheatley",
+  "Ralph Waldo Emerson",
+  "Richard Crashaw",
+  "Richard Lovelace",
+  "Robert Browning",
+  "Robert Burns",
+  "Robert Herrick",
+  "Robert Louis Stevenson",
+  "Robert Southey",
+  "Robinson",
+  "Rupert Brooke",
+  "Samuel Coleridge",
+  "Samuel Johnson",
+  "Sarah Flower Adams",
+  "Sidney Lanier",
+  "Sir John Suckling",
+  "Sir Philip Sidney",
+  "Sir Thomas Wyatt",
+  "Sir Walter Raleigh",
+  "Sir Walter Scott",
+  "Stephen Crane",
+  "Thomas Campbell",
+  "Thomas Chatterton",
+  "Thomas Flatman",
+  "Thomas Gray",
+  "Thomas Hood",
+  "Thomas Moore",
+  "Thomas Warton",
+  "Walt Whitman",
+  "Walter Savage Landor",
+  "Wilfred Owen",
+  "William Allingham",
+  "William Barnes",
+  "William Blake",
+  "William Browne",
+  "William Cowper",
+  "William Cullen Bryant",
+  "William Ernest Henley",
+  "William Lisle Bowles",
+  "William Morris",
+  "William Shakespeare",
+  "William Topaz McGonagall",
+  "William Vaughn Moody",
+  "William Wordsworth",
+];
 
 export default function Index() {
-  const [poems, setPoems] = useState<Poem[]>([]);
+  const [poem, setPoem] = useState<Poem | null>(null);
   const [linesShown, setLinesShown] = useState<number>(1);
-  const [correctPoemIndex, setCorrectPoemIndex] = useState<
-    number | undefined
-  >();
+  const [possibleAuthors, setPossibleAuthors] = useState<string[]>([]);
   const [score, setScore] = useState<number>(0);
   const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
   let scrollViewRef = useRef<ScrollView | null>(null);
-  const fallbackPoetsRef = useRef<Set<string>>(
-    new Set([
-      "Amy Levy",
-      "John Milton",
-      "Geoffrey Chaucer",
-      "Alexander Pope",
-      "Percy Bysshe Shelley",
-      "Isaac Watts",
-      "Edgar Allan Poe",
-    ]),
-  );
 
   const fetchPoetry = useCallback(() => {
-    fetch("https://poetrydb.org/random/4")
+    fetch("https://poetrydb.org/random/1")
       .then((res) => res.json())
       .then((data) => {
-        const correctAnswerIndex = getRandomInteger(data.length);
-        for (let i = 0; i < data.length; i++) {
-          if (
-            i !== correctAnswerIndex &&
-            data.some(
-              (poem: Poem, index: number) =>
-                poem.author === data[i].author && index !== i,
-            )
-          ) {
-            const pool = [...fallbackPoetsRef.current];
-            data[i].author = pool[getRandomInteger(pool.length)];
-          }
+        let currentIndex = poets.length;
+        // While there remain elements to shuffle...
+        while (currentIndex !== 0) {
+          // Pick a remaining element...
+          let randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+          // And swap it with the current element.
+          [poets[currentIndex], poets[randomIndex]] = [
+            poets[randomIndex],
+            poets[currentIndex],
+          ];
         }
-        for (const poem of data) {
-          fallbackPoetsRef.current.add(poem.author);
+        let choices = poets.slice(0, 3);
+        choices.push(data[0].author);
+        currentIndex = choices.length;
+        // While there remain elements to shuffle...
+        while (currentIndex !== 0) {
+          // Pick a remaining element...
+          let randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+          // And swap it with the current element.
+          [choices[currentIndex], choices[randomIndex]] = [
+            choices[randomIndex],
+            choices[currentIndex],
+          ];
         }
-        setPoems(data);
-        setCorrectPoemIndex(correctAnswerIndex);
+        setPoem(data[0]);
+        setPossibleAuthors(choices);
       });
   }, []);
 
@@ -60,21 +185,20 @@ export default function Index() {
     fetchPoetry();
   }, [fetchPoetry]);
 
-  const handleButtonPress = () => {
-    if (!correctPoemIndex) return;
+  const addLine = () => {
+    if (!poem) return;
 
-    if (poems[correctPoemIndex].lines[linesShown + 1]) {
+    if (poem?.lines[linesShown + 1]) {
       setLinesShown((prev) => prev + 1);
     } else {
       setLinesShown((prev) => prev + 2);
     }
   };
 
-  const selectPoet = (index: number) => {
-    setIsCorrect(index === correctPoemIndex);
-    setPoems([]);
+  const selectPoet = (author: string) => {
+    setIsCorrect(author === poem?.author);
+    setPoem(null);
     fetchPoetry();
-    setCorrectPoemIndex(undefined);
     setLinesShown(1);
     setTimeout(() => {
       setIsCorrect(null);
@@ -91,7 +215,7 @@ export default function Index() {
     );
   }
 
-  if (poems.length === 0 || correctPoemIndex === undefined) {
+  if (poem === null) {
     return (
       <View style={styles.loadingContainer}>
         <Text>Loading...</Text>
@@ -99,8 +223,7 @@ export default function Index() {
     );
   }
 
-  const isNewLineButtonEnabled =
-    linesShown < poems[correctPoemIndex].lines.length;
+  const isNewLineButtonEnabled = linesShown < poem?.lines.length;
 
   return (
     <View style={styles.container}>
@@ -115,29 +238,27 @@ export default function Index() {
         style={{ marginBottom: 24 }}
         showsVerticalScrollIndicator={false}
       >
-        {poems[correctPoemIndex].lines
-          .slice(0, linesShown)
-          .map((line, index) => (
-            <Text key={index} style={{ marginBottom: 6, fontSize: 16 }}>
-              {line.trim()}
-            </Text>
-          ))}
+        {poem?.lines.slice(0, linesShown).map((line, index) => (
+          <Text key={index} style={{ marginBottom: 6, fontSize: 16 }}>
+            {line.trim()}
+          </Text>
+        ))}
       </ScrollView>
       <View style={{ gap: 5 }}>
-        {poems.map((poem, index) => (
+        {possibleAuthors.map((author, index) => (
           <Pressable
             key={index}
             style={styles.poetChoiceButton}
             onPress={() => {
-              selectPoet(index);
+              selectPoet(author);
             }}
           >
-            <Text>{poem.author}</Text>
+            <Text>{author}</Text>
           </Pressable>
         ))}
       </View>
       <Pressable
-        onPress={handleButtonPress}
+        onPress={addLine}
         style={
           isNewLineButtonEnabled
             ? styles.newLineButton
